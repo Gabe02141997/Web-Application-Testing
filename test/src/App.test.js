4c45452d909f1d7573033717db3302feb3cd30a4
Gabriel Anguiano <anguianog97@gmail.com>
2019-12-05 15:59:52 -0800

    testing complete

--- test/src/App.test.js
+++ test/src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import Display from './components/Display';
 
-test('renders learn react link', () => {
+test('displays testing in app component', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/testing/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test('Displays Balls in Display component', () => {
+  const { getByText } = render(<Display/>)
+  const element = getByText(/balls:/i)
+  expect(element).toBeInTheDocument();
+})
+
+test('Displays strikes in Display Component', () => {
+  const { getByText } = render(<Display/>)
+  const element = getByText(/strikes:/i)
+  expect(element).toBeInTheDocument();
+})
